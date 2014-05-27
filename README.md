bc74feed3355fcb0dec5099f3b070f04640e3d07
Jonny Elliott <jonnyelliott@mpe.mpg.de>
2014-05-27 18:10:07 +0200

    First commit of files

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Excel file from Vlad:
+/diska/home/vsudilov/vss-flask/telbib-output.xlsx
+
+my data set is from Uta (sandra's wife), and it is only the bibliographic record of publications that referenced ESO instruments
+Vladimir Sudilovsky: from 2000-2013 iirc
+
---
+++ make_db.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+'''Pulled from Vlad's software: https://github.com/vsudilov/vss-flask/blob/master/utils/make_db.py'''
+
+import xlrd
+import time
+import unicodedata
+
+def sanitize(value):
+  if type(value)==unicode:
+    return unicodedata.normalize('NFKD',value.replace(u'\xc3\xbc','ue')).encode('ascii', 'ignore') #Manually put "ue" in u-umlaut...Need to use a better solution eventually
+  return value
+
+def main(db="/diska/home/jonny/sw/python/mlearning/data/telbib-output.xlsx"):
+  print "Reading in the excel document"
+  start = time.time()
+  wb = xlrd.open_workbook(db)
+  print "...done in %0.1f seconds" % (time.time()-start)
+  ws = wb.sheet_by_index(0)
+  header = ws.row(0)
+
+  print header
+
+if __name__=='__main__':
+  main()
