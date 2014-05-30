de369e77c36744f2d528605329e2383da9c60af6
Jonny Elliott <jonnyelliott@mpe.mpg.de>
2014-05-30 16:33:48 +0200

    Created array for collection wanted data. Began implementation of database creation. General house keeping updated

---
+++ TODO.md
@@ -0,0 +1,15 @@
+make_db.py
+----------
+
+Generate the database, and the code to populate the database with the following input
+
+Collate the following output into a single database:
+
+                         Done     Notes
+
+Citation count           No       -
+PubYear                  No       -
+Number of authors        No       -
+Length of abstract       No       -
+Length of title          No       -
+Number of telescopes     No       -
--- cite_learning/make_db.py
+++ cite_learning/make_db.py
@@ -1,10 +1,62 @@
 #!/usr/bin/env python
 
-'''Pulled from Vlad's software: https://github.com/vsudilov/vss-flask/blob/master/utils/make_db.py'''
+'''Pulled from Vlad's software: https://github.com/vsudilov/vss-flask/blob/master/utils/make_db.py
+
+Header information in the Excel file:
+
+text:u'BibCode'
+text:u'CitationCount'
+text:u'PubYear'
+text:u'AuthorName'
+text:u'AuthorRank'
+text:u'Journal'
+text:u'Telescope'
+text:u'Affiliation'
+text:u'Title'
+text:u'Abstract'
+
+Wanted output:
+
+Citation count      
+PubYear
+Number of authors
+Length of abstract
+Length of title
+Number of telescopes
+
+'''
 
 import xlrd
 import time
 import unicodedata
+import numpy
+
+def make_table(dbout="cilrn.db"):
+  try:
+    con = lite.connect(dbout)
+
+    with con:
+      cur = con.cursor()
+      cur.execute("CREATE TABLE CiteLearning (Id INTEGER PRIMARY KEY, BibCode TEXT, CitationCount INTEGER, PubYear INTEGER, LengthOfAbstract INTEGER, LengthOfTitle INTEGER, NumberOfAuthors INTEGER);")
+      # Layout: ID number, 
+      path = ("\'Test path\'",)
+      cur.execute("INSERT INTO Copied(Path) VALUES (?);", path)
+
+  except lite.Error, e:
+    if con:
+      con.rollback()
+      print "Error %s:" % e.args[0]
+      sys.exit(1)
+
+  finally:
+    if con:
+      con.close()
+
+def add_content(inDictionary):
+  con = lite.connect('test.db')
+  with con:
+    con.execute("INSERT INTO CiteLearning(BibCode,CitationCount) VALUES (?),(?);", (inDictionary,))
+  con.commit()
 
 def sanitize(value):
   if type(value)==unicode:
@@ -20,6 +72,55 @@ def main(db="/diska/home/jonny/sw/python/cite_learning/data/telbib-output.xlsx")
   header = ws.row(0)
 
   print header
+
+  db_arr = {
+           "BibCode": [],
+           "CitationCount": [],
+           "PubYear": [],
+           "LengthOfAbstract": [],
+           "LengthOfTitle": [],
+           "NumberOfAuthors": [],
+           "AuthorRank": [],
+          }
+
+
+  for i in range(ws.nrows-1):
+    loadvalue = float(i)/ws.nrows*100.0
+    if not round(loadvalue) % 10:
+      print "Loading: %0.1f%%" % (loadvalue)
+    row = ws.row(i+1)
+    BibCode,CitationCount,PubYear,Author,AuthorRank,Journal,Telescope,Affiliation,Title,Abstract = [sanitize(j.value) for j in row]
+
+    db_arr["CitationCount"].append(CitationCount)
+    db_arr["PubYear"].append(PubYear)
+    db_arr["LengthOfAbstract"].append(len(Abstract))
+    db_arr["LengthOfTitle"].append(len(Title))
+    db_arr["BibCode"].append(BibCode)
+    db_arr["NumberOfAuthors"].append(0)
+    db_arr["AuthorRank"].append(AuthorRank)
+
+    if i == 20:
+      break
+
+  # Convert to numpy arrays
+  for item in db_arr:
+    db_arr[item] = numpy.array(db_arr[item])
+
+  # Calculate number of authors per bibcode
+  for bibcode in list(set(db_arr["BibCode"])):
+    idx = db_arr["BibCode"] == bibcode
+   
+    db_arr["NumberOfAuthors"][idx] = len(db_arr["BibCode"][idx])
+    
+
+    # Write a single entry for this BibCode
+    for item in db_arr:
+      print item, db_arr[item][idx][0], db_arr[item][idx][-1]
+    print "\n\n"
+     
+
+
+  
 
 if __name__=='__main__':
   main()
