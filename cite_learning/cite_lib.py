d8ca9d593ba9389d2d78cd4f5af1ccdbd53980be
Jonny Elliott <jonnyelliott@mpe.mpg.de>
2014-08-05 10:41:45 +0200

    Commits that were not commited - doh!

--- cite_learning/cite_lib.py
+++ cite_learning/cite_lib.py
@@ -11,6 +11,7 @@ __version__ = "1.0"
 __email__ = "jonnynelliott@googlemail.com"
 __status__ = "Development"
 
+import numpy
 import sqlite3 as lite
 import unicodedata
 
@@ -68,6 +69,161 @@ def sanitize(value):
   if type(value)==unicode:
     return unicodedata.normalize('NFKD',value.replace(u'\xc3\xbc','ue')).encode('ascii', 'ignore') #Manually put "ue" in u-umlaut...Need to use a better solution eventually
   return value
+
+
+###################
+# Linear Regression
+
+def gradient_descent(x, theta, y, alpha=0.3):
+
+  # Normal gradient descent
+  # m - number of entries
+  # n - number of features
+
+  m, n = x.shape
+  theta -= (alpha/m) * numpy.dot((numpy.dot(theta.T, x.T).T - y).T, x).T
+
+  return theta
+
+
+def cost_function(x, theta, y):
+  
+  # Assumes a linear combination: f(x) = x1*theta1 + x2*theta2 + .... + xn*thetan
+  # m - number of entries
+  # n - number of features
+  m, n = x.shape
+
+  j = ((1./(2.*m)) * (h_x(x, theta) - y) ** 2.).sum()
+
+  return j
+
+
+def h_x(x, theta):
+  
+  h_x = numpy.dot(theta.T, x.T).T
+  return h_x
+
+  
+def feature_scaling(x):
+
+  x_mean = numpy.mean(x, axis=0)
+  x_std = numpy.std(x, axis=0)
+
+  x -= x_mean
+  x /= x_std
+
+  #print "Feature mean (average): %s" % x_mean.T
+  #print "Feature standard deviation: %s" % x_std.T
+
+  return x, x_mean, x_std
+
+##############################
+# Principal Component Analysis
+
+def svd_pca(X, k=2):
+
+  # Convert input array into matrix 
+  Xorig = numpy.matrix(X)
+
+  # Normalise and scale
+  X = (Xorig - Xorig.mean(0)) / (Xorig.std(0))
+  m, n = X.shape
+  print "\nCreating initial matrix"
+  print "Matrix X.shape: %sx%s\n" % X.shape
+
+  # Create the covariance matrix - see notes on SVD
+  #Sigma = (1/m) * (X.T * X)
+  #print "Creating co-variance matrix"
+  #print "Sigma S.shape: %sx%s\n" % Sigma.shape
+
+  # Single Value Decomposition
+  U, S, V = numpy.linalg.svd(X.T, full_matrices=True, compute_uv=True)
+
+  # Deconstruction 2D -> 1D
+  print "Determining U vector from SVD"
+  print "Matrix U, shape: %sx%s\n" % U.shape
+
+  z = (U[:,0:k].T * X.T).T
+  zorig = z
+
+  # Rescale and renormalise
+  print "Scaling and normalising back to original format"
+  print "Matrix z, shape: %sx%s\n" % z.shape
+  z = numpy.multiply(z, Xorig.std(0)[:,0:k]) + Xorig.mean(0)[:,0:k]
+  x_approx = numpy.multiply(zorig*U[:,0:k].T, Xorig.std(0)) + Xorig.mean(0)
+
+  z = numpy.array(z)
+  # For now, let's just return the decomposed dimensions
+  return z, S #, x_approx, zorig, Xorig, U
+
+
+
+
+
+
+
+
+
+###################
+# Data sets to test
+
+# Vlad's ESO data
+
+def load_data_vlad():
+
+  # Define arrays and variables
+  feature_matrix, measured_matrix = [], []
+
+  # Load in the full database
+  print "Loading data from SQL database..."
+  out1 = check_content(dbin="cilrn.db", search="PubYear,LengthOfAbstract,\
+                       LengthOfTitle,NumberOfAuthors")
+  out2 = check_content(dbin="cilrn.db", search="CitationCount")
+
+  # Create an array
+  for entry in out1:
+    feature_matrix.append([j for j in entry])
+
+  for entry in out2:
+    measured_matrix.append([j for j in entry])
+
+  feature_matrix = numpy.array(feature_matrix)
+  measured_matrix = numpy.array(measured_matrix)
+
+  return feature_matrix, measured_matrix
+
+
+# Kiplinger's school data: http://www.kiplinger.com/tool/college/T014-S001-kiplinger-s-best-values-in-private-colleges/index.php
+
+def load_data_kiplingers():
+  ofile = open("schools.txt", "r")
+  lines = ofile.readlines()
+  ofile.close()
+
+  M_output = []
+
+  for i in range(len(lines)):
+    j = [k for k in lines[i].strip().replace(",","").split(" ") if k]
+    name = " ".join(j[1:-9])
+    relevant = j[-9:-1]
+
+    # Relevant: ['TX', '17', '6', '84', '52,741', '31,820', '12,068', '26']
+    temp_arr = [name] + relevant
+
+    temp_arr = [float(temp_arr[h]) for h in range(len(temp_arr)) if h>1]
+
+    M_output.append(temp_arr)
+
+  M_output = numpy.array(M_output)
+
+  return M_output
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
--- cite_learning/learn.py
+++ cite_learning/learn.py
@@ -16,69 +16,13 @@ matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
 import numpy
 
-from cite_lib import check_content
-
-def gradient_descent(x, theta, y, alpha=0.3):
-
-  # Normal gradient descent
-  # m - number of entries
-  # n - number of features
-
-  m, n = x.shape
-  theta -= (alpha/m) * numpy.dot((numpy.dot(theta.T, x.T).T - y).T, x).T
-
-  return theta
-
-
-def cost_function(x, theta, y):
-  
-  # Assumes a linear combination: f(x) = x1*theta1 + x2*theta2 + .... + xn*thetan
-  # m - number of entries
-  # n - number of features
-  m, n = x.shape
-
-  j = ((1./(2.*m)) * (h_x(x, theta) - y) ** 2.).sum()
-
-  return j
-
-def h_x(x, theta):
-  
-  h_x = numpy.dot(theta.T, x.T).T
-  return h_x
-  
-def feature_scaling(x):
-
-  x_mean = numpy.mean(x, axis=0)
-  x_std = numpy.std(x, axis=0)
-
-  x -= x_mean
-  x /= x_std
-
-  #print "Feature mean (average): %s" % x_mean.T
-  #print "Feature standard deviation: %s" % x_std.T
-
-  return x, x_mean, x_std
+from cite_lib import check_content, gradient_descent, \
+                     cost_function, h_x, feature_scaling, \
+                     load_data_vlad
 
 def main():
 
-  # Define arrays and variables
-  feature_matrix, measured_matrix = [], []
-
- 
-  # Load in the full database
-  print "Loading data from SQL database..."
-  out1 = check_content(dbin="cilrn.db", search="PubYear,LengthOfAbstract,LengthOfTitle,NumberOfAuthors")
-  out2 = check_content(dbin="cilrn.db", search="CitationCount")
-
-  # Create an array
-  for entry in out1:
-    feature_matrix.append([j for j in entry])
-
-  for entry in out2:
-    measured_matrix.append([j for j in entry])
-  
-  feature_matrix = numpy.array(feature_matrix)
-  measured_matrix = numpy.array(measured_matrix)
+  feature_matrix, measured_matrix = load_data_vlad()
 
   feature_matrix, mean, std = feature_scaling(feature_matrix)
   print "Feature matrix mean: %s"  % mean
@@ -92,6 +36,12 @@ def main():
   print "Number of data entries (rows): %d\nNumber of features (columns):  %d" % feature_matrix.shape
 
 
+  print "Randomly shuffling matrix..."
+  shuffle_matrix = numpy.concatenate((measured_matrix, feature_matrix), axis=1)
+  numpy.random.shuffle(shuffle_matrix)
+  measured_matrix = numpy.array(shuffle_matrix[:,0:1])
+  feature_matrix = shuffle_matrix[:,1:]
+
   # Machine Learning
   # Linear Regression - albeit the data does not look so linear by itself
   # Parameter matrix
@@ -99,29 +49,47 @@ def main():
   print "Parameter matrix (rows):       %d\n              (columns):       %d" % parameter_matrix.shape
 
 
+  print "Seprating into learning data set and cross validation data set..."
+  m, n = feature_matrix.shape
+  lrn_m = int(numpy.ceil(0.6*m))
+  cross_validation_x = feature_matrix[lrn_m:,:]
+  cross_validation_y = measured_matrix[lrn_m:,:]
+
+  feature_matrix = feature_matrix[0:lrn_m,:]
+  measured_matrix = measured_matrix[0:lrn_m,:]
+  print "New feature matrix:          %d x %d" % (feature_matrix.shape)
+  print "New cross validation matrix: %d x %d" % (cross_validation_x.shape)
+
   print "Calculating the cost function..."
   J_theta = cost_function(feature_matrix, parameter_matrix, measured_matrix)
   print "...J(theta): %f" % J_theta
 
   print "Gradient descent..."
   print "initial theta: %s" % parameter_matrix.T
-  for i in range(1,200):
+
+  J_lrn, J_cv = [], []
+
+  num_iter = 200
+  for i in range(1,num_iter):
    
     parameter_matrix = gradient_descent(feature_matrix, parameter_matrix, measured_matrix, alpha=0.1)
-    J_theta = cost_function(feature_matrix, parameter_matrix, measured_matrix)
- 
-    print "   i: %d" % i
-    print "   theta: %s" % parameter_matrix.T
-    print "   cost: %f" % J_theta
-    print ""
+    J_lrn.append(cost_function(feature_matrix, parameter_matrix, measured_matrix))
+    J_cv.append(cost_function(cross_validation_x, parameter_matrix, cross_validation_y))
+
+
+  J_lrn, J_cv = numpy.array(J_lrn), numpy.array(J_cv)
+
   print "...end"
   # Load figure canvas
 
   fig = plt.figure(0)
   ax1 = fig.add_subplot(111)
-  ax1.errorbar(feature_matrix[:,3], measured_matrix, fmt="o", color="blue")
-  ax1.set_xlabel("Publication year")
-  ax1.set_ylabel("Citation count")
+  ax1.errorbar(range(1,num_iter), J_lrn, fmt="o", ls="-", label="J learn", color="blue")
+  ax1.errorbar(range(1,num_iter), J_cv, fmt="o", ls="-", label="J cv", color="red")
+  ax1.set_xlabel("Number of iterations")
+  ax1.set_ylabel("Cost function")
+
+  leg1 = ax1.legend(loc=0, numpoints=1, scatterpoints=1)
 
   plt.show()
 
